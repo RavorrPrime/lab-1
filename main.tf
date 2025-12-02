1d6a55b2510d56d069798bc6bacd00b3a8ce0ac9
Mateusz Tomaszewski <mateusz.tomaszewski49@wp.pl>
2025-12-02 14:59:18 +0000

    Changed main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,40 @@
 variable "server-name" {
-    type = string
-    description = "Name of a server to provision"
+  type        = string
+  default     = "kot"
+  description = "Name of a server to provision"
+}
 
+locals {
+  minNumberOfServer = 1
+  maxNumberOfServer = 10
 }
 
 variable "number-of-servers" {
-    type = number
-    description = "Required number of servers"
-
+  type        = number
+  description = "Required number of servers"
+  default     = 2
+  validation {
+    condition     = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
+    error_message = "Not supported number of server, it should be from the range ${local.minNumberOfServer} <= ${var.number-of-servers} < ${local.maxNumberOfServer}"
+  }
 }
 
-output "out" {
-    value = var.server-name
+variable "nubmer-of-disks" {
+  type = number
+}
+
+variable "list-of-names" {
+  type = list(string)
+}
+
+output "result" {
+  value = "${var.server-name} x ${var.number-of-servers}"
+}
+
+output "number-of-resources" {
+  value = var.nubmer-of-disks + var.number-of-servers
+}
+
+output "list-of-names" {
+  value = "${join(", ",var.list-of-names)}"
 }
