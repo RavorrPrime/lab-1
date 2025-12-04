366af881e9ea919df939c2096b8ca9246cc44348
Mateusz Tomaszewski <mateusz.tomaszewski49@wp.pl>
2025-12-04 23:22:39 +0000

    Changed main.tf (Homework)

--- main.tf
+++ main.tf
@@ -1,40 +1,24 @@
-variable "server-name" {
-  type        = string
-  default     = "kot"
-  description = "Name of a server to provision"
-}
-
-locals {
-  minNumberOfServer = 1
-  maxNumberOfServer = 10
-}
-
-variable "number-of-servers" {
-  type        = number
-  description = "Required number of servers"
-  default     = 2
-  validation {
-    condition     = var.number-of-servers >= local.minNumberOfServer && var.number-of-servers < local.maxNumberOfServer
-    error_message = "Not supported number of server, it should be from the range ${local.minNumberOfServer} <= ${var.number-of-servers} < ${local.maxNumberOfServer}"
-  }
-}
-
-variable "nubmer-of-disks" {
+variable "a" {
   type = number
 }
 
-variable "list-of-names" {
-  type = list(string)
+variable "b" {
+  type = number
+}
+
+variable "sign" {
+  type = string
+}
+
+locals {
+  operations = {
+   "+" = var.a + var.b
+   "-" = var.a - var.b
+   "*" = var.a * var.b
+   "/" = var.b == 0 ? "Error" : var.a / var.b
+  }
 }
 
 output "result" {
-  value = "${var.server-name} x ${var.number-of-servers}"
-}
-
-output "number-of-resources" {
-  value = var.nubmer-of-disks + var.number-of-servers
-}
-
-output "list-of-names" {
-  value = "${join(", ",var.list-of-names)}"
+  value = lookup(local.operations,var.sign)
 }
